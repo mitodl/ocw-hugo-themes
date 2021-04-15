0be903b3e7f575fe4d3b91d27d358da04aa7a768
Carey P Gumaer <gumaerc@mit.edu>
2021-04-15 14:45:18 -0400

    fix build script

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -11,10 +11,10 @@ fi
 npm run build:webpack
 if [[ -z "${EXTERNAL_SITE_PATH}" ]]; then
   # Build the site without external content
-  hugo -d dist -v
+  hugo -s base-theme -d dist -v
 else
   # Prep our external site to use this theme
-  /bin/bash assets/build_scripts/prep_external_site.sh
+  /bin/bash base-theme/assets/build_scripts/prep_external_site.sh
   # Build the site
   cd $EXTERNAL_SITE_PATH
   hugo -d $PWD/dist -v
