cfbb208c37b70bdbd80bc611d7f7127011f8fe32
Carey P Gumaer <gumaerc@mit.edu>
2021-04-21 16:59:25 -0400

    clean up example configs and fix course build script

--- build_scripts/start_course.sh
+++ build_scripts/start_course.sh
@@ -14,8 +14,8 @@ else
   rm -f private/test_course.json
   echo "{\"courses\":[\"${OCW_TEST_COURSE}\"]}" | tee private/test_course.json
   /bin/bash build_scripts/import_courses.sh  private/test_course.json
-  cp example.go.mod private/ocw-to-hugo-output/$OCW_TEST_COURSE/go.mod
-  cp example.config.toml private/ocw-to-hugo-output/$OCW_TEST_COURSE/config/_default/config.toml
+  cp example_configs/example.course.go.mod private/ocw-to-hugo-output/$OCW_TEST_COURSE/go.mod
+  cp example_configs/example.course.config.toml private/ocw-to-hugo-output/$OCW_TEST_COURSE/config/_default/config.toml
   cd private/ocw-to-hugo-output/$OCW_TEST_COURSE
   hugo mod get -u
   hugo server -p 3000 --bind 0.0.0.0 --renderToDisk
