15998e383ea694dc6e7683340e8d94fe7759a473
Carey Gumaer <gumaerc@mit.edu>
2021-05-05 17:59:17 -0400

    move pdfjs static build to the www theme so it's built with the main site, and only copy the files into a course build if it's running locally for development

--- build_scripts/start_course.sh
+++ build_scripts/start_course.sh
@@ -13,12 +13,15 @@ if [[ -z "${OCW_TEST_COURSE}" ]]; then
   echo "OCW_TEST_COURSE not set"
   exit 1
 else
+  rm -rf private/ocw-to-hugo-output/$OCW_TEST_COURSE
   mkdir -p private
   rm -f private/test_course.json
   echo "{\"courses\":[\"${OCW_TEST_COURSE}\"]}" | tee private/test_course.json
   /bin/bash build_scripts/import_courses.sh  private/test_course.json
   cp example_configs/example.course.go.mod private/ocw-to-hugo-output/$OCW_TEST_COURSE/go.mod
   cp example_configs/example.course.config.toml private/ocw-to-hugo-output/$OCW_TEST_COURSE/config/_default/config.toml
+  mkdir -p private/ocw-to-hugo-output/$OCW_TEST_COURSE/static/pdfjs
+  cp -r www/static/pdfjs/* private/ocw-to-hugo-output/$OCW_TEST_COURSE/static/pdfjs
   cd private/ocw-to-hugo-output/$OCW_TEST_COURSE
   hugo mod get -u
   hugo server -p 3000 --bind 0.0.0.0 --renderToDisk
