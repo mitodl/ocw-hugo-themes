e35328a14fe248bbab5bca8a7a4c74184cf85122
Carey Gumaer <gumaerc@mit.edu>
2021-05-10 17:08:40 -0400

    move over code from https://github.com/mitodl/ocw-course-hugo-theme/pull/87

--- course/assets/js/responsive_tables.js
+++ course/assets/js/responsive_tables.js
@@ -1,12 +1,12 @@
-// set data-title attribute on table cells
-function responsiveTables() {
+function initResponsiveTables() {
   const tables = document
-    .getElementById("main-content-wrapper")
+    .getElementById("main-content")
     .getElementsByTagName("table")
   for (const table of tables) {
     const headings = table.getElementsByTagName("th")
     const tbody = table.getElementsByTagName("tbody")[0]
     const rows = tbody.getElementsByTagName("tr")
+    // set data-title attribute on table cells
     for (const row of rows) {
       const cells = row.getElementsByTagName("td")
       for (let i = 0; i < cells.length; i++) {
@@ -18,6 +18,12 @@ function responsiveTables() {
         }
       }
     }
+    // force mobile style if table width exceeds main content area
+    const tableWidth = table.clientWidth
+    const mainContentWidth = document.getElementById("main-content").clientWidth
+    if (tableWidth > mainContentWidth) {
+      table.classList.add("mobile-table")
+    }
   }
 }
-responsiveTables()
+initResponsiveTables()
