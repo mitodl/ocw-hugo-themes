0bb60260c79f0f3b6b82cd890538d9214c6a9faa
Carey Gumaer <gumaerc@mit.edu>
2021-10-22 16:20:40 -0400

    set defaults for base url variables and break up hugo commands for readability

--- package_scripts/start_course.sh
+++ package_scripts/start_course.sh
@@ -6,10 +6,19 @@ source package_scripts/common.sh
 load_env --require-dot-env
 
 THEMES_PATH=$(pwd)
+STATIC_API_BASE_URL=${STATIC_API_BASE_URL:-}
+RESOURCE_BASE_URL=${RESOURCE_BASE_URL:-}
 COURSE_HUGO_CONFIG_PATH=${COURSE_HUGO_CONFIG_PATH:-}
 COURSE_CONTENT_PATH=${COURSE_CONTENT_PATH:-}
 OCW_TEST_COURSE=${OCW_TEST_COURSE:-}
 
+# If the base URL variables aren't set, assign defaults
+if [[ -z "${STATIC_API_BASE_URL}" ]]; then
+  export STATIC_API_BASE_URL="https://ocw-live-qa.global.ssl.fastly.net/"
+fi
+if [[ -z "${STATIC_API_BASE_URL}" ]]; then
+  export RESOURCE_BASE_URL="https://ol-ocw-studio-app-qa.s3.amazonaws.com/"
+fi
 if [[ -z "${COURSE_HUGO_CONFIG_PATH}" ]]; then
   # Path to Hugo config not set, fetch default from Github
   clone_or_pull_repo git@github.com:mitodl/ocw-hugo-projects.git private/ocw-hugo-projects
@@ -33,4 +42,9 @@ fi
 
 cd $COURSE_CONTENT_PATH
 # Run hugo server
-hugo server -p 3000 --bind 0.0.0.0 --config $COURSE_HUGO_CONFIG_PATH --themesDir $THEMES_PATH --renderToDisk
+hugo server \
+  -p 3000 \
+  --bind 0.0.0.0 \
+  --config $COURSE_HUGO_CONFIG_PATH \
+  --themesDir $THEMES_PATH \
+  --renderToDisk
--- package_scripts/start_www.sh
+++ package_scripts/start_www.sh
@@ -6,17 +6,33 @@ source package_scripts/common.sh
 load_env --require-dot-env
 
 THEMES_PATH=$(pwd)
+SEARCH_API_URL=${SEARCH_API_URL:-}
+OCW_STUDIO_BASE_URL=${OCW_STUDIO_BASE_URL:-}
 WWW_HUGO_CONFIG_PATH=${WWW_HUGO_CONFIG_PATH:-}
 WWW_CONTENT_PATH=${WWW_CONTENT_PATH:-}
 
+# If the base URL variables aren't set, assign defaults
+if [[ -z "${SEARCH_API_URL}" ]]; then
+  export SEARCH_API_URL="https://discussions-rc.odl.mit.edu/api/v0/search/"
+fi
+if [[ -z "${OCW_STUDIO_BASE_URL}" ]]; then
+  export OCW_STUDIO_BASE_URL="http://ocw-studio-rc.odl.mit.edu/"
+fi
 if [[ -z "${WWW_HUGO_CONFIG_PATH}" ]]; then
+  # Path to Hugo config not set, fetch default from Github
   clone_or_pull_repo git@github.com:mitodl/ocw-hugo-projects.git private/ocw-hugo-projects
   WWW_HUGO_CONFIG_PATH=$THEMES_PATH/private/ocw-hugo-projects/ocw-www/config.yaml
 fi
 if [[ -z "${WWW_CONTENT_PATH}" ]]; then
+  # Path to www content not set, fetch default from ocw-content-rc
   clone_or_pull_repo git@github.mit.edu:ocw-content-rc/ocw-www.git private/ocw-www
   WWW_CONTENT_PATH=$THEMES_PATH/private/ocw-www
 fi
 # Change to our content directory and start the Hugo server
 cd $WWW_CONTENT_PATH
-hugo server -p 3000 --bind 0.0.0.0 --config $WWW_HUGO_CONFIG_PATH --themesDir $THEMES_PATH --renderToDisk
+hugo server \
+  -p 3000 \
+  --bind 0.0.0.0 \
+  --config $WWW_HUGO_CONFIG_PATH \
+  --themesDir $THEMES_PATH \
+  --renderToDisk
