b3653df0b8dfa02b20236e271704e98f5dac0063
Muhammad Abdurrehman <muhammad.abdurrehman@arbisoft.com>
2022-01-05 14:36:50 +0500

    changes in design

--- www/assets/js/subnav.js
+++ www/assets/js/subnav.js
@@ -1,6 +1,6 @@
 const initSubNav = () => {
   // [START] Scrolling to appropriate section when subnav item is clicked
-  $(".on-page-sub-nav .nav-link").on("click", (e) => {
+  $(".on-page-sub-nav .nav-link").on("click", e => {
     const navbar = $(".navbar")
     const clickedLink = $(e.target)
     navbar.find(".active").removeClass("active")
@@ -10,23 +10,23 @@ const initSubNav = () => {
     const yOffset = -(navbar.height() + 20)
     const y = target.getBoundingClientRect().top + window.scrollY + yOffset
     window.scrollTo({
-      top: y,
+      top:      y,
       behavior: "smooth",
     })
   })
   // [END] Scrolling to appropriate section when subnav item is clicked
 
   // [START] highlight subnav item as user scrolls
-  $(window).on("scroll", function () {
+  $(window).on("scroll", function() {
     const position = window.pageYOffset
-    $(".section").each(function () {
+    $(".section").each(function() {
       const target = $(this).offset().top
       const navHeight = $(".on-page-sub-nav").height()
       if (position >= target - navHeight - 50) {
         const navLinks = $(".on-page-sub-nav  li a")
         const id = $(this).attr("id")
         navLinks.removeClass("active")
-        $('.on-page-sub-nav  li a[href="#' + id + '"]').addClass("active")
+        $(`.on-page-sub-nav  li a[href="#${  id  }"]`).addClass("active")
       }
     })
   })
