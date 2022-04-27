944972bc13c5258ff3a369af7c29704e77c57dd5
Muhammad Abdurrehman <muhammad.abdurrehman@arbisoft.com>
2022-04-27 15:23:18 +0500

    fix: try catch added

--- www/assets/js/lib/api.ts
+++ www/assets/js/lib/api.ts
@@ -2,21 +2,26 @@ import { buildSearchQuery, SearchQueryParams } from "./search"
 import { isApiSuccessful } from "./util"
 
 export const search = async (params: SearchQueryParams) => {
-  const body = buildSearchQuery(params)
-
-  const response = await fetch(process.env.SEARCH_API_URL!, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: new Headers({
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    })
-  })
-
   let results: any = {}
-  if (isApiSuccessful(response.status)) {
-    results = await response.json()
-  } else {
+
+  try {
+    const body = buildSearchQuery(params)
+
+    const response = await fetch(process.env.SEARCH_API_URL!, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      })
+    })
+
+    if (isApiSuccessful(response.status)) {
+      results = await response.json()
+    } else {
+      results["apiFailed"] = true
+    }
+  } catch (e) {
     results["apiFailed"] = true
   }
 
