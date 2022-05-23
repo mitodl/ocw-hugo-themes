640d113759abd5f4f5f09dac6ebc62ab314198fd
Wassaf-Shahzad <wassaf.shahzad@arbisoft.com>
2022-05-23 16:31:50 +0500

    added icon menue button

--- course/assets/course.js
+++ course/assets/course.js
@@ -11,6 +11,7 @@ import { initDesktopCourseInfoToggle } from "./js/course_info_toggle"
 import { initDivToggle } from "./js/div_toggle"
 import { initCourseInfoExpander } from "./js/course_expander"
 import { initVideoTranscriptTrack } from "./js/video_transcript_track"
+import { initSettingButton } from "./js/settings"
 import {
   clearSolution,
   checkAnswer,
@@ -22,6 +23,7 @@ $(function() {
   initDesktopCourseInfoToggle()
   initCourseInfoExpander(document)
   initVideoTranscriptTrack()
+  initSettingButton()
   initDivToggle()
   clearSolution()
   checkAnswer()
