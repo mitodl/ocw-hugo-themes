f01fe906a248c0d9532627d299c19da233950232
Abdurrehman <93309234+MAbdurrehman12@users.noreply.github.com>
2022-06-02 20:20:07 +0500

    resources layout moved to base theme from courses (#713)
    * resources layout moved to base-theme from courses
    
    * fix: formatting
    
    * addressed feedback
    
    * min-height added for pdf in www
    
    * fix: formatting

--- course/assets/course.js
+++ course/assets/course.js
@@ -6,7 +6,7 @@ import "nanogallery2/src/jquery.nanogallery2.core.js"
 import "./css/course.scss"
 import "videojs-youtube"
 
-import { initPdfViewers } from "./js/pdf_viewer"
+import { initPdfViewers } from "../../base-theme/assets/js/pdf_viewer"
 import { initDesktopCourseInfoToggle } from "./js/course_info_toggle"
 import { initDivToggle } from "./js/div_toggle"
 import { initCourseInfoExpander } from "./js/course_expander"
