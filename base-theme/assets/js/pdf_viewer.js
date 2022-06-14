25677d35ecdf99e9a81123daba3096c4303b10dd
Abdurrehman <93309234+MAbdurrehman12@users.noreply.github.com>
2022-06-14 10:51:37 +0500

    fix: catching exception while fetching PDF (#733)
    * fix: catching exception while fetching PDF
    
    * console.log replaced with console.error

--- base-theme/assets/js/pdf_viewer.js
+++ base-theme/assets/js/pdf_viewer.js
@@ -3,19 +3,23 @@
 /* eslint-disable no-console */
 export const initPdfViewers = () => {
   $(".pdf-wrapper").each(async (index, pdfWrapper) => {
-    const pdfUrl = pdfWrapper.dataset.pdfurl
-    const response = await window.fetch(pdfUrl)
-    if (response.status !== 200) {
-      console.error(`Problem fetching PDF: ${response.status}`)
-      return
+    try {
+      const pdfUrl = pdfWrapper.dataset.pdfurl
+      const response = await window.fetch(pdfUrl)
+      if (response.status !== 200) {
+        console.error(`Problem fetching PDF: ${response.status}`)
+        return
+      }
+      const blob = await response.blob()
+      const blobURI = window.URL.createObjectURL(blob)
+      const viewerUrl = `/pdfjs/web/viewer.html?file=${encodeURIComponent(
+        blobURI
+      )}`
+      $(pdfWrapper).html(
+        `<iframe class="w-100 h-100" frameborder="0" scrolling="yes" seamless="seamless" src="${viewerUrl}"></iframe>`
+      )
+    } catch (e) {
+      console.error("Exception occurred while fetching PDF: ", e)
     }
-    const blob = await response.blob()
-    const blobURI = window.URL.createObjectURL(blob)
-    const viewerUrl = `/pdfjs/web/viewer.html?file=${encodeURIComponent(
-      blobURI
-    )}`
-    $(pdfWrapper).html(
-      `<iframe class="w-100 h-100" frameborder="0" scrolling="yes" seamless="seamless" src="${viewerUrl}"></iframe>`
-    )
   })
 }
