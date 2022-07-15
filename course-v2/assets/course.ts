bee0cbdc02ef7db7b0456450b2f31273285eab58
Muhammad Abdurrehman <muhammad.abdurrehman@arbisoft.com>
2022-07-15 13:05:33 +0500

    course v2 added

---
+++ course-v2/assets/course.ts
@@ -0,0 +1,31 @@
+import "../../node_modules/nanogallery2/src/css/nanogallery2.css"
+
+import "offcanvas-bootstrap/dist/js/bootstrap.offcanvas.js"
+import "nanogallery2/src/jquery.nanogallery2.core.js"
+
+import "./css/course.scss"
+
+import { initDivToggle } from "./js/div_toggle"
+import { initCourseInfoExpander } from "./js/course_expander"
+import { initVideoTranscriptTrack } from "./js/video_transcript_track"
+import { initPlayBackSpeedButton } from "./js/video_playback_speed"
+import { initVideoFullscreenToggle } from "./js/video_fullscreen_toggle"
+import { initDownloadButton } from "./js/video-download-button"
+import {
+  clearSolution,
+  checkAnswer,
+  showSolution
+} from "./js/quiz_multiple_choice"
+import "videojs-youtube"
+
+$(function() {
+  initCourseInfoExpander(document)
+  initDownloadButton()
+  initPlayBackSpeedButton()
+  initVideoTranscriptTrack()
+  initDivToggle()
+  clearSolution()
+  checkAnswer()
+  showSolution()
+  initVideoFullscreenToggle()
+})
