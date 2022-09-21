33922aaedc4f70d6d1b3e291432d8f075f982e53
Carey Gumaer <gumaerc@mit.edu>
2022-09-21 18:54:10 -0400

    expand parent nav item when selected

--- course-v2/assets/js/navigation.js
+++ course-v2/assets/js/navigation.js
@@ -1,3 +1,12 @@
+// @ts-ignore
+function expandNav(navItemEl, collapseEl) {
+  collapseEl.classList.add("show")
+  // @ts-ignore
+  navItemEl
+    .querySelector(".course-nav-section-toggle, video-tab-toggle-section")
+    .setAttribute("aria-expanded", "true")
+}
+
 function courseNav() {
   document
     .querySelectorAll(".course-nav, .transcript-header")
@@ -13,6 +22,19 @@ function courseNav() {
           window.location.pathname.replace(/\/$/, "")
         ) {
           navLinkEl.classList.add("active")
+          const uuid = navLinkEl.dataset.uuid
+          const navItemEl = navLinkEl.closest(".course-nav-list-item")
+          const collapseEl = navItemEl?.querySelector(".collapse")
+          const sectionToggles = Array.prototype.filter.call(
+            document.querySelectorAll(".course-nav-section-toggle"),
+            node => {
+              return node.dataset.uuid === uuid
+            }
+          )
+          // if this is a parent item, expand its children
+          if (navItemEl && collapseEl && sectionToggles.length > 0) {
+            expandNav(navItemEl, collapseEl)
+          }
         }
       })
 
@@ -22,13 +44,7 @@ function courseNav() {
       navEl.querySelectorAll(".course-nav-list-item").forEach(navItemEl => {
         const collapseEl = navItemEl.querySelector(".collapse")
         if (collapseEl && collapseEl.contains(activeEl)) {
-          collapseEl.classList.add("show")
-          // @ts-ignore
-          navItemEl
-            .querySelector(
-              ".course-nav-section-toggle, video-tab-toggle-section"
-            )
-            .setAttribute("aria-expanded", "true")
+          expandNav(navItemEl, collapseEl)
         }
       })
     })
