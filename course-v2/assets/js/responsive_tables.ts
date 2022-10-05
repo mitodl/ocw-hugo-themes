fa32549b832471f1fd3dabeb190c95dc817ff55e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-10-05 14:32:41 -0400

    make v2 tables resize-sensitive

--- course-v2/assets/js/responsive_tables.ts
+++ course-v2/assets/js/responsive_tables.ts
@@ -1,25 +1,43 @@
+const getTables = () => document.querySelectorAll<HTMLElement>("#course-content-section table")
+const getCourseContent = () => document.getElementById("course-content-section")
+
 function initResponsiveTables2() {
-  const tables = document.querySelectorAll('#course-content-section table')
+  const tables = getTables()
+  const mainContent = getCourseContent()
+  if (!mainContent) return
+  if (!window.ResizeObserver) return
+
+  const observer = new ResizeObserver(() => calculateOverlap2())
+  observer.observe(mainContent)
   tables.forEach(table => {
-    const headings = table.querySelectorAll<HTMLElement>("thead th")
-    const rows = table.querySelectorAll("tbody tr")
+    const headings = table.getElementsByTagName("th")
+    const rows = table.querySelectorAll("table tr")
     // set data-title attribute on table cells
     rows.forEach(row => {
       const cells = row.querySelectorAll("td")
       cells.forEach((cell, i) => {
         if (headings.length >= i + 1) {
-          cells[i].setAttribute(
-            "data-title",
-            headings[i].innerText.trim().concat(": ")
-          )
+          cells[i].dataset["title"] = headings[i].innerText.trim().concat(": ")
         }
       })
     })
-    // force mobile style if table width exceeds main content area
-    const tableWidth = table.clientWidth
-    const mainContentWidth = document.getElementById("course-content-section")
-      ?.clientWidth ?? 0
-    if (tableWidth > mainContentWidth) {
+  })
+}
+
+function calculateOverlap2() {
+  const tables = getTables()
+  const mainContent = getCourseContent()
+  if (!mainContent) return
+
+  tables.forEach(table => {
+    const mainRect = mainContent.getBoundingClientRect()
+    const tableMinWidth = +(table.dataset.minWidth ?? 0)
+    if (mainRect.width > tableMinWidth) {
+      table.classList.remove('mobile-table')
+    }
+    const tableRect = table.getBoundingClientRect()
+    if (tableRect.right > mainRect.right) {
+      table.dataset.minWidth = `${tableRect.width}`
       table.classList.add("mobile-table")
     }
   })
