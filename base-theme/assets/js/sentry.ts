d763bce00c91102744bb49e5879ce6745d31dc3b
Abdurrehman <93309234+MAbdurrehman12@users.noreply.github.com>
2022-11-03 00:47:36 +0500

    fix: sentry DSN configured via env (#903)
    * fix: local errors logged to sentry
    
    * also checked hugo dev server
    
    * beforesend used instead of setting null to window.sentry
    
    * sentry file converted to typescript
    
    * some changes
    
    * rolling back
    
    * fix
    
    * sentry dsn configured via env
    
    * Revert "sentry dsn configured via env"
    
    This reverts commit cacd5e14d6f59bbbbf160773da95582218629264.
    
    * sentry dsn configured via env
    
    * fix: removed unused code
    
    * beforesend removed

--- base-theme/assets/js/sentry.ts
+++ base-theme/assets/js/sentry.ts
@@ -1,10 +1,9 @@
 import * as Sentry from "@sentry/browser"
 
-export const initSentry = () => {
+export const initSentry = (): typeof Sentry => {
   Sentry.init({
     release: RELEASE_VERSION,
-    dsn:
-      "https://eee58f41dda54d2b814296e12dced4b7@o48788.ingest.sentry.io/5304953"
+    dsn:     process.env.SENTRY_DSN
   })
   return Sentry
 }
