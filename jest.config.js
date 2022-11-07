619670ffe19fa09e4e38420295bca72148fb1646
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-11-07 16:16:21 -0500

    lint more things
    Let's lint+format all the non-dist js files. I'm about to add a ts file at project root, and a ts file under package_scripts/ that would not otherwise be linted.

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,13 @@ module.exports = {
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ["<rootDir>www/assets/js/test_setup.ts"],
 
-  watchPlugins:       [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/", "tests-e2e",
-  ],
-  preset:          'ts-jest',
-  testEnvironment: "jsdom"
+  testPathIgnorePatterns: ["/node_modules/", "tests-e2e"],
+  preset:                 "ts-jest",
+  testEnvironment:        "jsdom"
 }
