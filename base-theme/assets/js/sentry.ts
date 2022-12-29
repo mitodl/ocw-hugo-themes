fdde386b1a77324baa0e8e53a46e7534054f853f
Wassaf-Shahzad <wassaf.shahzad@arbisoft.com>
2022-12-29 13:44:18 +0500

    fixed linting issue

--- base-theme/assets/js/sentry.ts
+++ base-theme/assets/js/sentry.ts
@@ -1,10 +1,9 @@
 import * as Sentry from "@sentry/browser"
-import { env } from "../../../env"
 
 export const initSentry = (): typeof Sentry => {
   Sentry.init({
-    release: RELEASE_VERSION,
-    dsn:     process.env.SENTRY_DSN,
+    release:     RELEASE_VERSION,
+    dsn:         process.env.SENTRY_DSN,
     environment: process.env.SENTRY_ENV
   })
   return Sentry
