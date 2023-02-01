d14414b0989e9d95d46e73add6bc3e0d60964c69
Carey P Gumaer <gumaerc@mit.edu>
2023-02-01 17:22:48 -0500

    add ocw-course-v3 and course-offline-v2 (#1068)
    * duplicate course-offline in course-offline-v2
    
    * push up course-v2-relative folder
    
    * duplicate course-v2 into course-v3

---
+++ course-v3/assets/js/video_fullscreen_toggle.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+export const initVideoFullscreenToggle = () => {
+  // this fixes the issue of control bar going to the top on fullscreen
+  $(document).on(
+    "mozfullscreenchange webkitfullscreenchange fullscreenchange",
+    function() {
+      const fullscreenElement =
+        document.fullscreenElement ||
+        document.mozFullScreenElement ||
+        document.webkitFullscreenElement ||
+        document.msFullscreenElement
+      if (fullscreenElement) {
+        // entering full screen
+        $(".vjs-control-bar").css("bottom", 0)
+      } else {
+        // exiting full screen
+        $(".vjs-control-bar").css("bottom", "auto")
+      }
+    }
+  )
+}
---
+++ course-v3/content/learning_resource_types/_index.md
@@ -0,0 +1,5 @@
+---
+title: Resources
+url: /download/
+kind: taxonomy
+---
