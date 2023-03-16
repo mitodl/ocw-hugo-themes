d067ae723ce7dcc75177fdc2e085eeb79a4415eb
IbrahimJaved <ibrahimjaved@A0066-00600.local>
2023-03-16 13:00:04 +0500

    try to pass IE 11

--- course-v2/assets/videojs-imports.js
+++ course-v2/assets/videojs-imports.js
@@ -4,8 +4,6 @@ import { initVideoFullscreenToggle } from "./js/video_fullscreen_toggle"
 import { initDownloadButton } from "./js/video-download-button"
 
 import "videojs-youtube"
-// @ts-ignore
-window.Promise = undefined
 import "promise-polyfill/src/polyfill"
 
 export const initVideoJS = () => {
