5c4ad151b0059b9e96ffe11b977915e7b0323081
Carey Gumaer <gumaerc@mit.edu>
2023-03-21 13:41:20 -0400

    use specific www_offline bundle, which includes base www and search styles, and don't import lunr for now

--- www-offline/assets/www-offline.tsx
+++ www-offline/assets/www-offline.tsx
@@ -1,15 +1,13 @@
+import "./css/www-offline.scss"
 import Popper from "popper.js"
-var lunr = require("lunr")
 
 export interface OCWWindow extends Window {
   $: JQueryStatic
   jQuery: JQueryStatic
   Popper: typeof Popper
-  lunr: typeof lunr
 }
 declare let window: OCWWindow
 
 window.jQuery = $
 window.$ = $
 window.Popper = Popper
-window.lunr = lunr
