7907230119324b061ba342c9195954386e4732cd
IbrahimJaved <ibrahimjaved@A0066-00600.local>
2023-03-29 16:47:08 +0500

    refactor videoToggleDownloadPopup function

--- course-v2/assets/videojs-imports.js
+++ course-v2/assets/videojs-imports.js
@@ -2,6 +2,7 @@ import { initVideoTranscriptTrack } from "./js/video_transcript_track"
 import { initPlayBackSpeedButton } from "./js/video_playback_speed"
 import { initVideoFullscreenToggle } from "./js/video_fullscreen_toggle"
 import { initDownloadButton } from "./js/video-download-button"
+import { videoToggleDownloadPopup } from "./js/video_toggle_download"
 
 import "videojs-youtube"
 
@@ -10,4 +11,5 @@ export const initVideoJS = () => {
   initPlayBackSpeedButton()
   initVideoTranscriptTrack()
   initVideoFullscreenToggle()
+  videoToggleDownloadPopup()
 }
