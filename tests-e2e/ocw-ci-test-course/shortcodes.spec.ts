f684d737c0e6508b7233d50584a68b322c422730
Muhammad Ghulam Fakhar ud din <muhammad.fakhar@arbisoft.com>
2023-05-05 15:34:34 +0500

    Linting issues resolved

--- tests-e2e/ocw-ci-test-course/shortcodes.spec.ts
+++ tests-e2e/ocw-ci-test-course/shortcodes.spec.ts
@@ -38,44 +38,44 @@ test("Related resources link to correct page", async ({ page }) => {
   expect(href).toBe("/courses/ocw-ci-test-course/resources/example_pdf/")
 })
 
-test("Start and end times does not exist", async({ page })=> {
+test("Start and end times does not exist", async ({ page }) => {
   const course = new CoursePage(page, "course")
   await course.goto("resources/ocw_test_course_mit8_01f16_l01v02_360p")
   const src = await page.locator('iframe.vjs-tech').getAttribute('src')
-  
+
   expect(src).not.toMatch(/.*?start=.*/)
   expect(src).not.toMatch(/.*?end=.*/)
 })
 
 
-test("Start time exists", async({ page })=> {
+test("Start time exists", async ({ page }) => {
   const course = new CoursePage(page, "course")
   await course.goto("resources/ocw_test_course_mit8_01f16_l01v01_360p")
   const src = await page.locator('iframe.vjs-tech').getAttribute('src')
   expect(src).toMatch(/.*?start=13.*/)
 })
 
-test("End time exists", async({ page })=> {
+test("End time exists", async ({ page }) => {
   const course = new CoursePage(page, "course")
   await course.goto("resources/ocw_test_course_mit8_01f16_l01v01_360p")
   const src = await page.locator('iframe.vjs-tech').getAttribute('src')
   expect(src).toMatch(/.*?end=15.*/)
 })
 
-test("Start and end time exists", async({ page })=> {
+test("Start and end time exists", async ({ page }) => {
   const course = new CoursePage(page, "course")
   const expectedStartTime = '13'
   const expectedEndTime = '15'
 
   await course.goto("resources/ocw_test_course_mit8_01f16_l01v01_360p")
   const src = await page.locator('iframe.vjs-tech').getAttribute('src')
-  
+
   const urlParams = new URLSearchParams(src || '')
   expect(urlParams.get('start')).toEqual(expectedStartTime)
   expect(urlParams.get('end')).toEqual(expectedEndTime)
 })
 
-test("Transcripts start time matches video start time", async({ page })=> {
+test("Transcripts start time matches video start time", async ({ page }) => {
   const course = new CoursePage(page, "course")
   const expectedStartTime = '13'
 
@@ -94,5 +94,4 @@ test("Transcripts start time matches video start time", async({ page })=> {
 
   expect(parseFloat(activeCaption || '0')).toBeLessThanOrEqual(parseFloat(expectedStartTime))
   expect(parseFloat(nextCaption || '0')).toBeGreaterThanOrEqual(parseFloat(expectedStartTime))
-
 })
