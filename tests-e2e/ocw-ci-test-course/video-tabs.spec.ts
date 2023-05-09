410890719f3bc36affac1740d3f4a0e3f4e2943f
Carey Gumaer <gumaerc@mit.edu>
2023-05-09 15:27:38 -0400

    review feedback

--- tests-e2e/ocw-ci-test-course/video-tabs.spec.ts
+++ tests-e2e/ocw-ci-test-course/video-tabs.spec.ts
@@ -1,26 +1,30 @@
 import { test, expect } from "@playwright/test"
-import { VideoPage } from "../util/VideoPage"
+import { VideoElement } from "../util/VideoElement"
+import { CoursePage } from "../util"
 
 test("A page without a transcript has the proper tab titles and contents", async ({
   page
 }) => {
-  const videoPage = new VideoPage(page)
-  await videoPage.goto("/resources/ocw_test_course_mit8_01f16_l01v01_360p")
+  const coursePage = new CoursePage(page, "course")
+  await coursePage.goto("/resources/ocw_test_course_mit8_01f16_l01v01_360p")
+  const videoPage = new VideoElement(page)
 
-  await expect(videoPage.tabs).toHaveText([
+  await expect(videoPage.tab({})).toHaveText([
     /\s*$/, // first tab is just download button with no title.
     /Related Resources/,
     /Optional Tab/
   ])
 
-  const panels = await videoPage.tabPanels.all()
+  const panels = await videoPage.tabPanel({ includeHidden: true }).all()
   const html = await Promise.all(panels.map(panel => panel.innerHTML()))
 
   expect(html).toHaveLength(3)
 
+  const emptyTabHTML = html[0]
   const relatedResourcesHTML = html[1]
   const optionalTabHTML = html[2]
 
+  expect(emptyTabHTML).toMatch("")
   expect(relatedResourcesHTML).toContain("Practice problems")
   expect(relatedResourcesHTML).toContain(
     '<a href="/courses/ocw-ci-test-course/resources/example_pdf/">(PDF)</a>'
@@ -31,7 +35,8 @@ test("A page without a transcript has the proper tab titles and contents", async
 })
 
 test("A page with a transcript has a transcript tab", async ({ page }) => {
-  const videoPage = new VideoPage(page)
-  await videoPage.goto("/resources/ocw_test_course_mit8_01f16_l01v02_360p")
-  await expect(page.getByRole("tab")).toHaveText(["Transcript"])
+  const coursePage = new CoursePage(page, "course")
+  await coursePage.goto("/resources/ocw_test_course_mit8_01f16_l01v02_360p")
+  const videoPage = new VideoElement(page)
+  await expect(videoPage.tab({})).toHaveText(["Transcript"])
 })
