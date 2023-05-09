7e37f760e4ae365c8bb275b1d31ae607e08697fc
Carey P Gumaer <gumaerc@mit.edu>
2023-05-09 15:41:14 -0400

    pass page context to video_expandable_tab wherever it's used (#1138)
    * anywhere video_expandable_tab.html is called as a partial, the context argument needs to be passed
    
    * upgrade playwright
    
    * add test to ensure that the expected expandable tabs show up on a video page
    
    * remove debug code
    
    * fix checking of transcript for existence
    
    * check if captions and transcript exist before rendering the exandable transcript tab, and add a test
    
    * assign roles and use getByRole
    
    * add a comment about the expected first blank tab
    
    * add basic skeleton of video page object model and update video tabs tests to use it
    
    * remove unnecessary await
    
    * review feedback

--- tests-e2e/ocw-ci-test-www/featured_courses.spec.ts
+++ tests-e2e/ocw-ci-test-www/featured_courses.spec.ts
@@ -4,9 +4,9 @@ import { WwwPage } from "../util"
 test("Course Card lists Instructors", async ({ page }) => {
   const www = new WwwPage(page)
   await www.goto()
-  const card = www
-    .getCourseCard("OCW CI Test Course", { expectedCount: 6 })
-    .first()
+  const card = (
+    await www.getCourseCard("OCW CI Test Course", { expectedCount: 6 })
+  ).first()
   await expect(card).toContainText(
     "Instructors(s): Prof. Tester One, Dr. Tester Two, Another Tester Three"
   )
