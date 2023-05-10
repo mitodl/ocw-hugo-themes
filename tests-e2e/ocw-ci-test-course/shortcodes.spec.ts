3b7f2428a35f84647e6fa7c194db09822b289e7e
Muhammad Ghulam Fakhar ud din <muhammad.fakhar@arbisoft.com>
2023-05-10 20:13:37 +0500

    Video code removed and moved to another file

--- tests-e2e/ocw-ci-test-course/shortcodes.spec.ts
+++ tests-e2e/ocw-ci-test-course/shortcodes.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test"
-import { CoursePage, VideoSection } from "../util"
+import { CoursePage } from "../util"
 
 test("Resource links titles render without extra spaces", async ({ page }) => {
   const course = new CoursePage(page, "course")
@@ -36,69 +36,4 @@ test("Related resources link to correct page", async ({ page }) => {
   })
   const href = await resourceLink.getAttribute("href")
   expect(href).toBe("/courses/ocw-ci-test-course/resources/example_pdf/")
-})
-
-test("Start and end times does not exist", async ({ page }) => {
-  const course = new CoursePage(page, "course")
-  await course.goto("resources/ocw_test_course_mit8_01f16_l01v02_360p")
-  const src = await page.locator("iframe.vjs-tech").getAttribute("src")
-
-  expect(src).not.toMatch(/.*?start=.*/)
-  expect(src).not.toMatch(/.*?end=.*/)
-})
-
-test("Start time exists", async ({ page }) => {
-  const course = new CoursePage(page, "course")
-  await course.goto("resources/ocw_test_course_mit8_01f16_l01v01_360p")
-  const src = await page.locator("iframe.vjs-tech").getAttribute("src")
-  expect(src).toMatch(/.*?start=13.*/)
-})
-
-test("End time exists", async ({ page }) => {
-  const course = new CoursePage(page, "course")
-  await course.goto("resources/ocw_test_course_mit8_01f16_l01v01_360p")
-  const src = await page.locator("iframe.vjs-tech").getAttribute("src")
-  expect(src).toMatch(/.*?end=50.*/)
-})
-
-test("Start and end time exists", async ({ page }) => {
-  const course = new CoursePage(page, "course")
-  const expectedStartTime = "13"
-  const expectedEndTime = "50"
-
-  await course.goto("resources/ocw_test_course_mit8_01f16_l01v01_360p")
-  const src = await page.locator("iframe.vjs-tech").getAttribute("src")
-
-  const urlParams = new URLSearchParams(src || "")
-  expect(urlParams.get("start")).toEqual(expectedStartTime)
-  expect(urlParams.get("end")).toEqual(expectedEndTime)
-})
-
-test("Transcripts start time matches video start time", async ({ page }) => {
-  const course = new CoursePage(page, "course")
-  const videoSection = new VideoSection(page)
-  const expectedStartTime = "13"
-
-  await course.goto("resources/ocw_test_course_mit8_01f16_l01v01_360p")
-  const src = await page.locator("iframe.vjs-tech").getAttribute("src")
-
-  const urlParams = new URLSearchParams(src || "")
-  expect(urlParams.get("start")).toEqual(expectedStartTime)
-
-  await videoSection.expectPlayerReady()
-  await videoSection.playButton().click()
-
-  const activeCaption = await videoSection.transcript
-    .activeLine()
-    .getAttribute("data-begin")
-  const nextCaption = await videoSection.transcript
-    .nextLine()
-    .getAttribute("data-begin")
-
-  expect(parseFloat(activeCaption || "0")).toBeLessThanOrEqual(
-    parseFloat(expectedStartTime)
-  )
-  expect(parseFloat(nextCaption || "0")).toBeGreaterThanOrEqual(
-    parseFloat(expectedStartTime)
-  )
 })
