cb2f05032d2191b181b0f848d112e316e36c1dd1
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2023-05-31 22:42:37 +0500

    Close popup when clicked on another videos download button

--- course-v2/assets/js/video_download_popup.ts
+++ course-v2/assets/js/video_download_popup.ts
@@ -1,20 +1,37 @@
 export const initVideoDownloadPopup = () => {
   const downloadIcons = document.querySelectorAll(".video-download-icons")
   const popups = document.querySelectorAll(".video-tab-download-popup")
+  let activePopup: HTMLElement | null = null
 
   if (downloadIcons.length > 0) {
     downloadIcons.forEach((downloadIcon, index) => {
-      const popup = popups[index]
+      const popup = popups[index] as HTMLElement
       downloadIcon.addEventListener("click", event => {
         event.stopPropagation()
-        if (popup) popup.classList.toggle("hidden")
+        if (popup) {
+          // Clicked on the same download button, toggle the popup
+          if (popup === activePopup) {
+            popup.classList.toggle("hidden")
+            activePopup = null
+          }
+          // Clicked on a different download button, close previous popup (if any) and toggle open new popup
+          else {
+            if (activePopup) {
+              activePopup.classList.add("hidden")
+            }
+            // Show the clicked popup
+            popup.classList.remove("hidden")
+            activePopup = popup
+          }
+        }
       })
     })
   }
-
+  // Click anywhere on page (other than download buttons), and the active popup will close
   document.addEventListener("click", () => {
-    popups.forEach(popup => {
-      popup.classList.add("hidden")
-    })
+    if (activePopup) {
+      activePopup.classList.add("hidden")
+      activePopup = null
+    }
   })
 }
