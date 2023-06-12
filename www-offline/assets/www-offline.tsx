230b925d446452b36d2f96881cdb271676d87158
Carey Gumaer <gumaerc@mit.edu>
2023-06-12 18:45:32 -0400

    add fuse and proof of concept of course search

--- www-offline/assets/www-offline.tsx
+++ www-offline/assets/www-offline.tsx
@@ -1,13 +1,16 @@
 import "./css/www-offline.scss"
+import Fuse from "fuse.js"
 import Popper from "popper.js"
 
 export interface OCWWindow extends Window {
   $: JQueryStatic
   jQuery: JQueryStatic
   Popper: typeof Popper
+  Fuse: typeof Fuse
 }
 declare let window: OCWWindow
 
 window.jQuery = $
 window.$ = $
 window.Popper = Popper
+window.Fuse = Fuse
---
+++ www-offline/content/_index.md
@@ -0,0 +1,3 @@
+---
+renderSearchIcon: true
+---
