d404dd19556a66e73d3326a9fcb5ef89e96c532d
pt2302 <1553279+pt2302@users.noreply.github.com>
2023-07-12 01:06:46 -0400

    Adding Playwright test to check download button exists

---
+++ tests-e2e/ocw-ci-test-course/download.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test"
+import { CoursePage } from "../util"
+
+test("Download button exists for test course", async ({ page }) => {
+  const course = new CoursePage(page, "course")
+  await course.goto('/download')
+  const downloadButton = page.getByRole("link", { name: "Download course"})
+  await expect(downloadButton).toBeVisible()
+})
