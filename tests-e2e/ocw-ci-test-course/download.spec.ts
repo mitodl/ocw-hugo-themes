d2bdb761f4c0e8afb536b221a2ad048f357904d5
pt2302 <1553279+pt2302@users.noreply.github.com>
2023-07-12 01:09:02 -0400

    Formatting

--- tests-e2e/ocw-ci-test-course/download.spec.ts
+++ tests-e2e/ocw-ci-test-course/download.spec.ts
@@ -3,7 +3,7 @@ import { CoursePage } from "../util"
 
 test("Download button exists for test course", async ({ page }) => {
   const course = new CoursePage(page, "course")
-  await course.goto('/download')
-  const downloadButton = page.getByRole("link", { name: "Download course"})
+  await course.goto("/download")
+  const downloadButton = page.getByRole("link", { name: "Download course" })
   await expect(downloadButton).toBeVisible()
 })
