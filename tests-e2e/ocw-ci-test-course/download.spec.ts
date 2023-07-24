03792bdf915c5ebdca779e7eccbf1ef11b0c7b50
pt2302 <1553279+pt2302@users.noreply.github.com>
2023-07-24 17:49:47 -0400

    Adding Playwright test for resources on download page

--- tests-e2e/ocw-ci-test-course/download.spec.ts
+++ tests-e2e/ocw-ci-test-course/download.spec.ts
@@ -7,3 +7,13 @@ test("Download button exists for test course", async ({ page }) => {
   const downloadButton = page.getByRole("link", { name: "Download course" })
   await expect(downloadButton).toBeVisible()
 })
+
+test("List of resources appears on download page", async ({ page }) => {
+  const course = new CoursePage(page, "course")
+  await course.goto("/download")
+  const tab = page.getByRole("link", { name: "Activity Assignments" })
+  await tab.click()
+  const resourceLink = page.getByRole("link", { name: "example_jpg.jpg" })
+  const href = await resourceLink.getAttribute("href")
+  expect(href).toBe("/courses/ocw-ci-test-course/resources/example_jpg/")
+})
