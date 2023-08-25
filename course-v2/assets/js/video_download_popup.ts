5d025eac8510bd873d6f9a063f8507647fd89182
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2023-08-25 18:28:36 +0500

    fix: accessibility of video tabs

--- course-v2/assets/js/video_download_popup.ts
+++ course-v2/assets/js/video_download_popup.ts
@@ -2,6 +2,7 @@ export const initVideoDownloadPopup = () => {
   const downloadIcons = document.querySelectorAll(".video-download-icons")
   const popups = document.querySelectorAll(".video-tab-download-popup")
   let activePopup: HTMLElement | null = null
+  let activePopupDownloadIconIndex = -1
 
   downloadIcons.forEach((downloadIcon, index) => {
     const popup = popups[index] as HTMLElement
@@ -9,22 +10,37 @@ export const initVideoDownloadPopup = () => {
       event.stopPropagation()
       if (popup === activePopup) {
         // Clicked on the same download button, toggle the popup
+        if (downloadIcon.getAttribute("aria-expanded") === "true") {
+          downloadIcon.setAttribute("aria-expanded", "false")
+        } else {
+          downloadIcon.setAttribute("aria-expanded", "true")
+        }
         popup.classList.toggle("hidden")
         activePopup = null
       } else {
         // Clicked on a different download button, close previous popup (if any) and toggle open new popup
         if (activePopup) {
+          downloadIcons[activePopupDownloadIconIndex].setAttribute(
+            "aria-expanded",
+            "false"
+          )
           activePopup.classList.add("hidden")
         }
         // Show the clicked popup
+        downloadIcon.setAttribute("aria-expanded", "true")
         popup.classList.remove("hidden")
         activePopup = popup
+        activePopupDownloadIconIndex = index
       }
     })
   })
   // Click anywhere on page (other than download buttons), and the active popup will close
   document.addEventListener("click", () => {
     if (activePopup) {
+      downloadIcons[activePopupDownloadIconIndex].setAttribute(
+        "aria-expanded",
+        "false"
+      )
       activePopup.classList.add("hidden")
       activePopup = null
     }
