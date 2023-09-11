c0417b1c64313ba903ce906bacea4f1e1cf14c7a
Hussain Taj <hussain.taj@arbisoft.com>
2023-09-11 12:57:36 +0500

    run fmt

--- tests-e2e/ocw-ci-test-course/course-info.spec.ts
+++ tests-e2e/ocw-ci-test-course/course-info.spec.ts
@@ -18,20 +18,22 @@ test("Course info section can be toggled", async ({ page }) => {
   await expect(heading).toBeVisible()
 })
 
-test("Toggling topics does not affect drawer layout", async ({page}) => {
+test("Toggling topics does not affect drawer layout", async ({ page }) => {
   const course = new CoursePage(page, "course")
   await course.goto("/pages/section-1")
 
   const heading = await page.getByRole("heading", { name: "Course Info" })
-  const topicCollapseButton = await page.getByRole('button', { name: 'Engineering subtopics' })
+  const topicCollapseButton = await page.getByRole("button", {
+    name: "Engineering subtopics"
+  })
 
   await expect(heading).toBeVisible()
   await expect(topicCollapseButton).toHaveAttribute("aria-expanded", "true")
-  
+
   await topicCollapseButton.click()
   const mainSection = await page.locator(`#${MAIN_COURSE_SECTION_ID}`)
   const drawer = await page.locator(`#${DESKTOP_COURSE_DRAWER_ID}`)
 
   await expect(mainSection).toHaveClass(/.*col-lg-9.*/)
-  await expect(drawer).toHaveClass(/.*col-3.*/)  
+  await expect(drawer).toHaveClass(/.*col-3.*/)
 })
