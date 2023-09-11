ea68380a062939e5e76bb05071e17c046ab936b8
Hussain Taj <hussain.taj@arbisoft.com>
2023-09-11 11:51:40 +0500

    add drawer test

--- tests-e2e/ocw-ci-test-course/course-info.spec.ts
+++ tests-e2e/ocw-ci-test-course/course-info.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test"
 import { CoursePage } from "../util"
 
+const DESKTOP_COURSE_DRAWER_ID = "desktop-course-drawer"
+const MAIN_COURSE_SECTION_ID = "main-course-section"
+
 test("Course info section can be toggled", async ({ page }) => {
   const course = new CoursePage(page, "course")
   await course.goto("/pages/section-1")
@@ -13,4 +16,22 @@ test("Course info section can be toggled", async ({ page }) => {
   await expect(heading).toBeHidden()
   await button.click()
   await expect(heading).toBeVisible()
+})
+
+test("Toggling topics does not affect drawer layout", async ({page}) => {
+  const course = new CoursePage(page, "course")
+  await course.goto("/pages/section-1")
+
+  const heading = await page.getByRole("heading", { name: "Course Info" })
+  const topicCollapseButton = await page.getByRole('button', { name: 'Engineering subtopics' })
+
+  await expect(heading).toBeVisible()
+  await expect(topicCollapseButton).toHaveAttribute("aria-expanded", "true")
+  
+  await topicCollapseButton.click()
+  const mainSection = await page.locator(`#${MAIN_COURSE_SECTION_ID}`)
+  const drawer = await page.locator(`#${DESKTOP_COURSE_DRAWER_ID}`)
+
+  await expect(mainSection).toHaveClass(/.*col-lg-9.*/)
+  await expect(drawer).toHaveClass(/.*col-3.*/)  
 })
