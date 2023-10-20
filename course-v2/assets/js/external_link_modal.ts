d9f3c1685f6ab0651ce989c952956369088dcaaa
Hussain Taj <hussain.taj@arbisoft.com>
2023-10-20 17:18:47 +0500

    add exceptions

--- course-v2/assets/js/external_link_modal.ts
+++ course-v2/assets/js/external_link_modal.ts
@@ -9,11 +9,21 @@ export function initExternalLinkModal() {
     // Close the drawer in mobile view before showing the modal.
     $(`#${MOBILE_COURSE_NAV_DRAWER_ID}`).trigger("offcanvas.close")
 
-    const targetUrl = event.target.getAttribute("href") ?? "#"
+    const targetUrl = $(event.currentTarget).data("target-url")
+    if (!targetUrl) {
+      throw Error("External link does not have a target.")
+    }
+
     const modal = $(`#${EXTERNAL_LINK_MODAL_ID}`)
 
     // Set the modal's "continue" link to the targetUrl.
-    modal.find("a.btn-continue").attr("href", targetUrl)
+    const continueButton = modal.find("a.btn-continue")
+
+    if (continueButton) {
+      continueButton.attr("href", targetUrl)
+    } else {
+      throw Error("Continue button was not found on the modal.")
+    }
 
     // @ts-ignore
     modal.modal("show")
