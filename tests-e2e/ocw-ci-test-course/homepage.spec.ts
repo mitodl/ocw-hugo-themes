ad80a4e8ad0312da4a0ace79f181be467e9d0dd4
Carey Gumaer <gumaerc@mit.edu>
2023-11-13 11:04:55 -0500

    use SITEMAP_DOMAIN in tests where it is expected

--- tests-e2e/ocw-ci-test-course/homepage.spec.ts
+++ tests-e2e/ocw-ci-test-course/homepage.spec.ts
@@ -65,6 +65,7 @@ test.describe("Course info", () => {
 })
 
 test("Has expected meta tags in <head>", async ({ page }) => {
+  const sitemapDomain = env.SITEMAP_DOMAIN ? env.SITEMAP_DOMAIN : "https://live-qa.ocw.mit.edu"
   const course = new CoursePage(page, "course")
   await course.goto()
   const metaShareImage = page.locator('meta[property="og:image"]')
@@ -73,12 +74,12 @@ test("Has expected meta tags in <head>", async ({ page }) => {
   const metaTwitterCard = page.locator('meta[name="twitter:card"]')
   await expect(metaShareImage).toHaveAttribute(
     "content",
-    "/courses/123-ocw-ci-test-course-fall-2022/example_jpg.jpg"
+    `https://${sitemapDomain}/courses/123-ocw-ci-test-course-fall-2022/example_jpg.jpg`
   )
   await expect(metaTwitterSite).toHaveAttribute("content", "@mitocw")
   await expect(metaTwitterImage).toHaveAttribute(
     "content",
-    "/courses/123-ocw-ci-test-course-fall-2022/example_jpg.jpg"
+    `${sitemapDomain}/courses/123-ocw-ci-test-course-fall-2022/example_jpg.jpg`
   )
   await expect(metaTwitterCard).toHaveAttribute(
     "content",
