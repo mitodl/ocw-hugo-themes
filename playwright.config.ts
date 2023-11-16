93531e961a4ac872d45a644379494862c01c468c
Carey Gumaer <gumaerc@mit.edu>
2023-11-16 18:28:13 -0500

    try upping timeout to 60 seconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ const config: PlaywrightTestConfig = {
   testDir:    "./tests-e2e",
   testIgnore: ["**/jest/**"],
   /* Maximum time one test can run for. */
-  timeout:    30 * 1000,
+  timeout:    60 * 1000,
   expect:     {
     timeout: 5000
   },
