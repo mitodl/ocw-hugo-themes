df559af81ccf68f69adecc58d4d892a933cad4b8
Hussain Taj <hussain.taj@arbisoft.com>
2024-01-11 17:09:39 +0500

    rewrite tests without navigation

--- tests-e2e/ocw-ci-test-www/homepage.spec.ts
+++ tests-e2e/ocw-ci-test-www/homepage.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test"
 import { WwwPage, siteUrl } from "../util"
+import { SOCIAL_LINKS } from "../util/constants"
 
 test("Home page has title in <head>", async ({ page }) => {
   await page.goto(siteUrl("www"))
@@ -11,30 +12,16 @@ test("Home page has title in <head>", async ({ page }) => {
 test("Social links exist and open correct social media pages", async ({
   page
 }) => {
-  const SOCIAL_LINKS = [
-    ["facebook", /https:\/\/www.facebook.com\/MITOCW\/?/],
-    ["instagram", /https:\/\/www.instagram.com\/mitocw\/?/],
-    ["twitter", /https:\/\/twitter.com\/MITOCW\/?/],
-    ["youtube", /https:\/\/www.youtube.com\/mitocw\/?/],
-    ["linkedin", /https:\/\/www.linkedin.com\/company\/mit-opencourseware\/?/]
-  ]
-
   const www = new WwwPage(page)
   await www.goto()
 
   for (const [name, url] of SOCIAL_LINKS) {
     for (const location of [".social-icon-row", "#footer-container"]) {
-      const popupPromise = page.waitForEvent("popup")
-
       const link = page
         .locator(location)
         .getByRole("link", { name, exact: true })
       await expect(link).toBeVisible()
-      await link.click()
-
-      const popup = await popupPromise
-      await popup.waitForURL(url, { timeout: 10000, waitUntil: "commit" })
-      await popup.close()
+      await expect(link).toHaveAttribute("href", url)
     }
   }
 })
---
+++ tests-e2e/util/constants.ts
@@ -0,0 +1,7 @@
+export const SOCIAL_LINKS = [
+  ["facebook", /^https:\/\/www.facebook.com\/MITOCW\/?$/],
+  ["instagram", /^https:\/\/www.instagram.com\/mitocw\/?$/],
+  ["twitter", /^https:\/\/twitter.com\/MITOCW\/?$/],
+  ["youtube", /^https:\/\/www.youtube.com\/mitocw\/?$/],
+  ["linkedin", /^https:\/\/www.linkedin.com\/company\/mit-opencourseware\/?$/]
+]
