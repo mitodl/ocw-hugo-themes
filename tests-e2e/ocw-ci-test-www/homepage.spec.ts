bac80c6e0ba57ce0c0be6f076c63cc5a75e9a74a
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2024-01-18 15:37:57 +0500

    feat: update social links (#1295)
    * update social links
    
    * rewrite tests without navigation
    
    * minify svgs
    
    * update alt for twitter or x

--- tests-e2e/ocw-ci-test-www/homepage.spec.ts
+++ tests-e2e/ocw-ci-test-www/homepage.spec.ts
@@ -1,9 +1,27 @@
 import { test, expect } from "@playwright/test"
-import { siteUrl } from "../util"
+import { WwwPage, siteUrl } from "../util"
+import { SOCIAL_LINKS } from "../util/constants"
 
-test("Course page have title in <head>", async ({ page }) => {
+test("Home page has title in <head>", async ({ page }) => {
   await page.goto(siteUrl("www"))
   await expect(page).toHaveTitle(
     "MIT OpenCourseWare | Free Online Course Materials"
   )
+})
+
+test("Social links exist and open correct social media pages", async ({
+  page
+}) => {
+  const www = new WwwPage(page)
+  await www.goto()
+
+  for (const [name, url] of SOCIAL_LINKS) {
+    for (const location of [".social-icon-row", "#footer-container"]) {
+      const link = page
+        .locator(location)
+        .getByRole("link", { name, exact: true })
+      await expect(link).toBeVisible()
+      await expect(link).toHaveAttribute("href", url)
+    }
+  }
 })
