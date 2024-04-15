b4ddc3dabb2ac965a383c5ed5c474908511ef4f0
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2024-04-15 14:20:45 +0500

    Bring videojs to base-theme

--- base-theme/assets/index.ts
+++ base-theme/assets/index.ts
@@ -7,7 +7,9 @@ import * as Sentry from "@sentry/browser"
 import { initSentry } from "./js/sentry"
 import PDFObject from "pdfobject"
 import "./js/polyfill"
+import "video.js/dist/video-js.css"
 import { initExternalLinkModal } from "./js/external_link_modal"
+import { initVideoDownloadPopup } from "./js/video_download_popup"
 
 export interface OCWWindow extends Window {
   $: JQueryStatic
@@ -15,6 +17,7 @@ export interface OCWWindow extends Window {
   Popper: typeof Popper
   Sentry: typeof Sentry
   PDFObject: typeof PDFObject
+  initVideoJS: () => void
 }
 
 declare let window: OCWWindow
@@ -23,6 +26,18 @@ window.jQuery = $
 window.$ = $
 window.Popper = Popper
 window.PDFObject = PDFObject
+
+let videoJSLoaded = false
+
+window.initVideoJS = () => {
+  if (videoJSLoaded) return
+  initVideoDownloadPopup()
+  import("./videojs-imports").then(module => {
+    module.initVideoJS()
+  })
+  videoJSLoaded = true
+}
+
 
 $(function() {
   window.Sentry = initSentry()
