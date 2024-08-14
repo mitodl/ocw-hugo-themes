c4f7543547f510c9fc5de5483a9a79b209623644
Carey Gumaer <gumaerc@mit.edu>
2024-08-14 14:01:29 -0400

    update package with mit-learn rename

---
+++ base-theme/assets/types/mit-learn-components.d.ts
@@ -0,0 +1,5 @@
+declare module 'mit-learn-components' {
+    declare const initMitOpenDom: (container: HTMLElement) => void
+    declare const openAddToUserListDialog: (readableId: string) => void
+    export { initMitOpenDom, openAddToUserListDialog }
+}
--- base-theme/assets/types/mit-open-components.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'mit-open-components' {
-    declare const initMitOpenDom: (container: HTMLElement) => void
-    declare const openAddToUserListDialog: (readableId: string) => void
-    export { initMitOpenDom, openAddToUserListDialog }
-}
