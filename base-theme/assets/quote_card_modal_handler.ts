43949ace24ecb305b7594efb9651267b928c6a90
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2024-11-26 23:22:30 +0500

    Use leadquote

--- base-theme/assets/quote_card_modal_handler.ts
+++ base-theme/assets/quote_card_modal_handler.ts
@@ -1,23 +1,20 @@
 export function initQuotesModalHandler() {
-  $(document).ready(() => {
-    const modalElement = $(".js-quote-modal")
+  const modalElement = $(".js-quote-modal")
+  $(".js-modal-trigger").on("click", event => {
+    event.preventDefault()
 
-    $(".js-modal-trigger").on("click", event => {
-      event.preventDefault()
+    const target = $(event.currentTarget)
+    const modalContentId = target.data("modal-content-id")
+    const contentDiv = $(`#${modalContentId}`)
 
-      const target = $(event.currentTarget)
-      const modalContentId = target.data("modal-content-id")
-      const contentDiv = $(`#${modalContentId}`)
-
-      if (contentDiv.length) {
-        const content = contentDiv.html()
-        modalElement.find(".modal-body.quote-modal-body").html(content)
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        modalElement.modal("show")
-      } else {
-        console.error("Modal content not found for ID:", modalContentId)
-      }
-    })
+    if (contentDiv.length) {
+      const content = contentDiv.html()
+      modalElement.find(".modal-body.quote-modal-body").html(content)
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      modalElement.modal("show")
+    } else {
+      console.error("Modal content not found for ID:", modalContentId)
+    }
   })
 }
