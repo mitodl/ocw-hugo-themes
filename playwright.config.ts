82651466105fb87ae1ded1f284254f50fa636951
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2025-01-22 12:46:44 +0500

    Increase test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ const config: PlaywrightTestConfig = {
   /* Maximum time one test can run for. */
   timeout:    60 * 1000,
   expect:     {
-    timeout: 5000
+    timeout: 10000
   },
   fullyParallel: true,
   forbidOnly:    !!process.env.CI,
