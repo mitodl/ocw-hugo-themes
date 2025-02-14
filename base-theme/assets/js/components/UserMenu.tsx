88b679691717bb79241926bb389b026fd437672a
Carey Gumaer <gumaerc@mit.edu>
2025-02-14 17:01:49 -0500

    fmt

--- base-theme/assets/js/components/UserMenu.tsx
+++ base-theme/assets/js/components/UserMenu.tsx
@@ -10,8 +10,14 @@ export default function UserMenu() {
   const apiBaseUrl = process.env.MIT_LEARN_API_BASEURL
   const encodedLocation = encodeURI(window.location.href)
   const myListsUrl = new URL("/dashboard/my-lists", learnBaseUrl).toString()
-  const logoutUrl = new URL(`/logout?next=${encodedLocation}`, apiBaseUrl).toString()
-  const loginUrl = new URL(`/login/ol-oidc?next=${encodedLocation}`, apiBaseUrl).toString()
+  const logoutUrl = new URL(
+    `/logout?next=${encodedLocation}`,
+    apiBaseUrl
+  ).toString()
+  const loginUrl = new URL(
+    `/login/ol-oidc?next=${encodedLocation}`,
+    apiBaseUrl
+  ).toString()
 
   return (
     <div className="dropdown">
@@ -24,37 +30,31 @@ export default function UserMenu() {
       >
         <RiAccountCircleFill size={24} />
       </button>
-      <div className="dropdown-menu dropdown-menu-right" aria-labelledby="userMenu">
+      <div
+        className="dropdown-menu dropdown-menu-right"
+        aria-labelledby="userMenu"
+      >
         {!isLoading ? (
           user?.isAuthenticated ? (
-          <>
-            <div className="px-3 py-2">
-              {user.first_name} {user.last_name}
-            </div>
-            <a
-              className="dropdown-item text-capitalize"
-              href={myListsUrl}
-            >
-              My Lists
+            <>
+              <div className="px-3 py-2">
+                {user.first_name} {user.last_name}
+              </div>
+              <a className="dropdown-item text-capitalize" href={myListsUrl}>
+                My Lists
+              </a>
+              <a className="dropdown-item text-capitalize" href={logoutUrl}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <a className="dropdown-item text-capitalize" href={loginUrl}>
+              Login
             </a>
-            <a
-              className="dropdown-item text-capitalize"
-              href={logoutUrl}
-            >
-              Logout
-            </a>
-          </>
+          )
         ) : (
-          <a
-            className="dropdown-item text-capitalize"
-            href={loginUrl}
-          >
-            Login
-          </a>
-        )
-      ) : (
-        <div className="dropdown-item text-capitalize">Loading...</div>
-      )}
+          <div className="dropdown-item text-capitalize">Loading...</div>
+        )}
       </div>
     </div>
   )
