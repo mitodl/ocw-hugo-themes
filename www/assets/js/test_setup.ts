11a3e445c91119813ee7214d32b8c8858d528994
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2025-02-19 16:12:52 +0500

    Upgrade React to v17

--- www/assets/js/test_setup.ts
+++ www/assets/js/test_setup.ts
@@ -1,7 +1,7 @@
 import { enableFetchMocks } from "jest-fetch-mock"
 enableFetchMocks()
 import Enzyme from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
 
 Enzyme.configure({ adapter: new Adapter() })
 
