4bd10ce65e762892d95f07dce983643e111347d6
ibrahimjaved12 <109785089+ibrahimjaved12@users.noreply.github.com>
2025-02-25 11:40:27 +0500

    Upgrade Hugo version to latest (0.142.0) (#1512)
    * Upgrade hugo version to 0.142.0
    
    * Replace verbose with logLevel info
    
    * Replace verbose with logLevel info in build.sh

--- package_scripts/start.ts
+++ package_scripts/start.ts
@@ -14,17 +14,16 @@ type HugoServerOptions = {
   bind: string
   config: string
   themesDir: string
-  renderToDisk: boolean
+  logLevel?: string
 }
 
 const hugoServer = (
   opts: Partial<HugoServerOptions> & Pick<HugoServerOptions, "config">
 ) => {
   const allOpts: HugoServerOptions = {
-    port:         3000,
-    bind:         "0.0.0.0",
-    themesDir:    process.cwd(),
-    renderToDisk: true,
+    port:      3000,
+    bind:      "0.0.0.0",
+    themesDir: process.cwd(),
     ...opts
   }
   return `hugo server ${u.getOptions(allOpts)}`
@@ -162,7 +161,13 @@ const makeEnvOpt = (
   ]
 }
 
-const start = program.description("Start ocw-hugo-themes development servers.")
+const start = program
+  .description("Start ocw-hugo-themes development servers.")
+  .option(
+    "--logLevel [value]",
+    "Set the log level for Hugo (options: debug, info, warn, error).",
+    "info"
+  )
 
 type StartOptions = {
   gitContentSource: string
@@ -194,10 +199,13 @@ const startSiteAction = async (opts: StartOptions, cliOptNames: string[]) => {
 
   await ensureHugoConfig("COURSE_HUGO_CONFIG_PATH")
 
+  const globalOpts = program.opts() as { logLevel: string }
+  const logLevel = globalOpts.logLevel
+
   startWebpackAnd({
     name:    "hugo",
     cwd:     opts.contentDir,
-    command: hugoServer({ config: opts.config })
+    command: hugoServer({ config: opts.config, logLevel })
   })
 }
 
