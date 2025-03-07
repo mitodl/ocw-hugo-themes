6b0bdee4ba8fa4ad4475cb89d2b1c0330a4f9b2e
pt2302 <1553279+pt2302@users.noreply.github.com>
2025-03-06 19:17:20 -0500

    Removing PostHog initialization from base theme

--- base-theme/assets/index.ts
+++ base-theme/assets/index.ts
@@ -11,15 +11,12 @@ import "video.js/dist/video-js.css"
 import { initExternalLinkModal } from "./js/external_link_modal"
 import { initQuotesModalHandler } from "./quote_card_modal_handler"
 import { initVideoDownloadPopup } from "./js/video_download_popup"
-import posthog from "posthog-js"
-
 export interface OCWWindow extends Window {
   $: JQueryStatic
   jQuery: JQueryStatic
   Popper: typeof Popper
   Sentry: typeof Sentry
   PDFObject: typeof PDFObject
-  posthog: typeof posthog
   initVideoJS: () => void
   initModalHandler: () => void
 }
