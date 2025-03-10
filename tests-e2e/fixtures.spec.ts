4437c0d1a5c90a1694f6b8032a05f1930ee0d208
ibrahimjaved12 <ibrahim.javed@arbisoft.com>
2025-03-10 15:11:01 +0500

    yarn fmt

--- tests-e2e/fixtures.spec.ts
+++ tests-e2e/fixtures.spec.ts
@@ -16,7 +16,7 @@ test("The fixtures are what Hugo would produce.", async () => {
      * the test homepage.
      */
     "courses/some-featured-course",
-    "courses/some-new-course",
+    "courses/some-new-course"
   ]
 
   const fixtureDir = path.resolve(__dirname, "../test-sites/__fixtures__")
