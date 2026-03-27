7d4016c42e2465ac2c23e9da576072e2c4aa8161
Umar Hassan <umar.hassan8@gmail.com>
2026-03-27 06:09:16 +0500

    chore: address PR review feedback
    - Remove unnecessary jquery resolution pin
    - Remove DISABLE_V8_COMPILE_CACHE=1 from lint script
    - Change images hash from fullhash to contenthash in webpack config

--- base-theme/assets/webpack/webpack.common.ts
+++ base-theme/assets/webpack/webpack.common.ts
@@ -79,7 +79,7 @@ const config: webpack.Configuration = {
         test:      /\.(?:jpg|png|svg|gif)$/,
         type:      "asset/resource",
         generator: {
-          filename: "images/[fullhash][ext]"
+          filename: "images/[contenthash][ext]"
         }
       },
       {
