f3e299fe6aedd8d0fa7b55f944f766677461e4ae
Umar Hassan <umar.hassan8@gmail.com>
2026-03-30 17:41:12 +0500

    chore: addressed feedback

--- tests-e2e/ocw-ci-test-course/homepage.spec.ts
+++ tests-e2e/ocw-ci-test-course/homepage.spec.ts
@@ -89,6 +89,20 @@ test("Has expected meta tags in <head>", async ({ page }) => {
   )
 })
 
+test("Appzi is only injected on the production sitemap domain", async ({ page }) => {
+  const course = new CoursePage(page, "course")
+  const sitemapDomain = env.SITEMAP_DOMAIN.replace(/\/$/, "")
+  await course.goto()
+
+  const appziScript = page.locator('script[src*="https://w.appzi.io/w.js"]')
+
+  if (sitemapDomain === "ocw.mit.edu") {
+    await expect(appziScript).toHaveCount(1)
+  } else {
+    await expect(appziScript).toHaveCount(0)
+  }
+})
+
 test("Course robots.txt allows crawling by default", async ({ page }) => {
   const sitemapDomain = env.SITEMAP_DOMAIN ?
     env.SITEMAP_DOMAIN :
