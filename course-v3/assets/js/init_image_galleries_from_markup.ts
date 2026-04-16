32109152e4f923fabf8f7e893eccc1602c14f2e7
Umar Hassan <umar.hassan8@gmail.com>
2026-04-16 07:50:47 +0500

    chore: meeting linter

--- course-v3/assets/js/init_image_galleries_from_markup.ts
+++ course-v3/assets/js/init_image_galleries_from_markup.ts
@@ -2,20 +2,20 @@ import { fastlyOptimizedUrl } from "../../../base-theme/assets/js/utils"
 
 export function initImageGalleriesFromMarkup() {
   const galleries = document.querySelectorAll(".image-gallery")
-  galleries.forEach((gallery) => {
+  galleries.forEach(gallery => {
     const baseUrl = gallery.getAttribute("data-base-url")
 
     const links = gallery.querySelectorAll("a[href]")
-    const items = Array.from(links).map((link) => {
+    const items = Array.from(links).map(link => {
       const src = link.getAttribute("href") || ""
       return {
         src,
         srct: fastlyOptimizedUrl(src, {
-          format: "avif",
+          format:  "avif",
           quality: "60",
-          width: "480",
+          width:   "480"
         }),
-        title: link.innerHTML,
+        title: link.innerHTML
       }
     })
 
@@ -42,9 +42,9 @@ export function initImageGalleriesFromMarkup() {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     window.jQuery(gallery).nanogallery2({
-      itemsBaseURL: baseUrl,
-      items: items,
-      allowHTMLinData: true,
+      itemsBaseURL:    baseUrl,
+      items:           items,
+      allowHTMLinData: true
     })
   })
 }
